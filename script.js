b4dcf73d33bb51c13ad6f41c39a328db637501a5
Kyriakos Poyias <kyriakos@poyias.com>
2023-04-01 19:08:22 +0300

    Default debug to false

--- script.js
+++ script.js
@@ -345,7 +345,7 @@ window.addEventListener("load", function () {
       this.gameTime = 0;
       this.timeLimit = 30000;
       this.speed = 1;
-      this.debug = true;
+      this.debug = false;
     }
     update(deltaTime) {
       if (!this.gameOver) this.gameTime += deltaTime;
