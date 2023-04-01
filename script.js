4403a2c89d7bcc5ac73b5e66031c9be3fc9e5fb1
Kyriakos Poyias <kyriakos@poyias.com>
2023-04-01 18:07:40 +0300

    Add animated Background

--- script.js
+++ script.js
@@ -127,9 +127,49 @@ window.addEventListener("load", function () {
     }
   }
 
-  class Layer {}
+  class Layer {
+    constructor(game, image, speedModifier) {
+      this.game = game;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.width = 1768;
+      this.height = 500;
+      this.x = 0;
+      this.y = 0;
+    }
+    update() {
+      // this.x -= this.game.backgroundSpeed * this.speedModifier;
+      if (this.x < -this.width) this.x = 0;
+      else this.x -= this.game.speed * this.speedModifier;
+    }
+    draw(context) {
+      context.drawImage(this.image, this.x, this.y);
+      // We draw the image twice to create a continuous background
+      context.drawImage(this.image, this.x + this.width, this.y);
+    }
+  }
 
-  class Background {}
+  class Background {
+    constructor(game) {
+      this.game = game;
+      this.image1 = document.getElementById("layer1");
+      this.image2 = document.getElementById("layer2");
+      this.image3 = document.getElementById("layer3");
+      this.image4 = document.getElementById("layer4");
+      this.layer1 = new Layer(this.game, this.image1, 0.2);
+      this.layer2 = new Layer(this.game, this.image2, 0.4);
+      this.layer3 = new Layer(this.game, this.image3, 1);
+      // Layer 4 is the foreground and should be drawn last after the player
+      this.layer4 = new Layer(this.game, this.image4, 1.5);
+      this.layers = [this.layer1, this.layer2, this.layer3];
+    }
+    update() {
+      this.layers.forEach((layer) => layer.update());
+    }
+    draw(context) {
+      this.layers.forEach((layer) => layer.draw(context));
+    }
+  }
 
   class UI {
     constructor(game) {
@@ -188,6 +228,7 @@ window.addEventListener("load", function () {
     constructor(width, height) {
       this.width = width;
       this.height = height;
+      this.background = new Background(this);
       this.player = new Player(this);
       this.inputHandler = new InputHandler(this);
       this.ui = new UI(this);
@@ -204,10 +245,13 @@ window.addEventListener("load", function () {
       this.winningScore = 10;
       this.gameTime = 0;
       this.timeLimit = 30000;
+      this.speed = 1;
     }
     update(deltaTime) {
       if (!this.gameOver) this.gameTime += deltaTime;
       if (this.gameTime > this.timeLimit) this.gameOver = true;
+      this.background.update();
+      this.background.layer4.update();
       this.player.update();
       if (this.ammoTimer > this.ammoInterval) {
         if (this.ammo < this.maxAmmo) this.ammo++;
@@ -243,9 +287,13 @@ window.addEventListener("load", function () {
       }
     }
     draw(context) {
+      // The order in which we draw things is important
+      // The background should be drawn first so that it is behind everything else
+      this.background.draw(context);
       this.player.draw(context);
       this.ui.draw(context);
       this.enemies.forEach((enemy) => enemy.draw(context));
+      this.background.layer4.draw(context);
     }
 
     addEnemy() {
