2ebe1521e5ca35ffad6c0fe6015e8f4876320da1
Kyriakos Poyias <kyriakos@poyias.com>
2023-04-01 19:03:15 +0300

    Add PowerUps

--- script.js
+++ script.js
@@ -68,9 +68,12 @@ window.addEventListener("load", function () {
       this.maxSpeed = 2;
       this.projectiles = [];
       this.image = document.getElementById("player");
+      this.powerUp = false;
+      this.powerUpTimer = 0;
+      this.powerUpLimit = 10000;
     }
 
-    update() {
+    update(deltaTime) {
       if (this.game.keys.includes("ArrowUp")) {
         this.speedY = -this.maxSpeed;
       } else if (this.game.keys.includes("ArrowDown")) {
@@ -87,11 +90,28 @@ window.addEventListener("load", function () {
       // Handle Sprite Animation
       if (this.frameX < this.maxFrame) this.frameX++;
       else this.frameX = 0;
+
+      // Power UP
+      if (this.powerUp) {
+        if (this.powerUpTimer > this.powerUpLimit) {
+          this.powerUpTimer = 0;
+          this.powerUp = false;
+          this.frameY = 0;
+        } else {
+          this.powerUpTimer += deltaTime;
+          this.frameY = 1;
+          this.game.ammo += 0.1;
+        }
+      }
     }
 
     draw(context) {
       if (this.game.debug)
         context.strokeRect(this.x, this.y, this.width, this.height);
+      // Draw Projectile
+      this.projectiles.forEach((projectile) => projectile.draw(context));
+
+      // Draw Player
       context.drawImage(
         this.image,
         this.frameX * this.width,
@@ -103,8 +123,6 @@ window.addEventListener("load", function () {
         this.width,
         this.height
       );
-      // Draw Projectile
-      this.projectiles.forEach((projectile) => projectile.draw(context));
     }
     shootTop() {
       if (this.game.ammo > 0) {
@@ -113,6 +131,21 @@ window.addEventListener("load", function () {
         );
         this.game.ammo--;
       }
+      if (this.powerUp) this.shootBottom();
+    }
+
+    shootBottom() {
+      if (this.game.ammo > 0) {
+        this.projectiles.push(
+          new Projectile(this.game, this.x + 80, this.y + 175)
+        );
+        this.game.ammo--;
+      }
+    }
+    enterPowerUp() {
+      this.powerUpTimer = 0;
+      this.powerUp = true;
+      this.game.ammo = this.game.maxAmmo;
     }
   }
 
@@ -252,10 +285,7 @@ window.addEventListener("load", function () {
       context.font = `${this.fontSize}px ${this.fontFamily}`;
       // Draw Score
       context.fillText(`Score: ${this.game.score}`, 20, 40);
-      // Draw Ammo
-      for (let i = 0; i < this.game.ammo; i++) {
-        context.fillRect(20 + 5 * i, 50, 3, 20);
-      }
+
       // Draw Timer
       const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
       context.fillText(`Timer: ${formattedTime}`, 20, 100);
@@ -284,7 +314,11 @@ window.addEventListener("load", function () {
           this.game.height / 2 + 40
         );
       }
-
+      // Draw Ammo
+      if (this.game.player.powerUp) context.fillStyle = "#ffffbd";
+      for (let i = 0; i < this.game.ammo; i++) {
+        context.fillRect(20 + 5 * i, 50, 3, 20);
+      }
       context.restore();
     }
   }
@@ -318,7 +352,7 @@ window.addEventListener("load", function () {
       if (this.gameTime > this.timeLimit) this.gameOver = true;
       this.background.update();
       this.background.layer4.update();
-      this.player.update();
+      this.player.update(deltaTime);
       if (this.ammoTimer > this.ammoInterval) {
         if (this.ammo < this.maxAmmo) this.ammo++;
         this.ammoTimer = 0;
@@ -329,6 +363,8 @@ window.addEventListener("load", function () {
         enemy.update();
         if (this.checkCollision(this.player, enemy)) {
           enemy.markedForDeletion = true;
+          if ((enemy.type = "lucky")) this.player.enterPowerUp();
+          else this.score--;
         }
         this.player.projectiles.forEach((projectile) => {
           if (this.checkCollision(projectile, enemy)) {
