463d693265aeea4b9fa8aa31ebedc3ba61b83056
Kyriakos Poyias <kyriakos@poyias.com>
2023-04-01 18:45:05 +0300

    Add Luckyfish

--- script.js
+++ script.js
@@ -178,6 +178,20 @@ window.addEventListener("load", function () {
       this.score = this.lives;
     }
   }
+
+  class LuckyFish extends Enemy {
+    constructor(game) {
+      super(game);
+      this.width = 99;
+      this.height = 95;
+      this.y = Math.random() * (this.game.height * 0.9 - this.height);
+      this.image = document.getElementById("lucky");
+      this.frameY = Math.floor(Math.random() * 2);
+      this.lives = 3;
+      this.score = 15;
+      this.type = "lucky";
+    }
+  }
   class Layer {
     constructor(game, image, speedModifier) {
       this.game = game;
@@ -350,8 +364,9 @@ window.addEventListener("load", function () {
 
     addEnemy() {
       const randomize = Math.random();
-      if (randomize < 0.5) this.enemies.push(new Angler1(this));
-      else this.enemies.push(new Angler2(this));
+      if (randomize < 0.3) this.enemies.push(new Angler1(this));
+      else if (randomize < 0.6) this.enemies.push(new Angler2(this));
+      else this.enemies.push(new LuckyFish(this));
     }
 
     checkCollision(rect1, rect2) {
